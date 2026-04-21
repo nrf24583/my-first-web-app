c76dc0fc58dd4c867e83d42fd83cd3c55cdb628b
nrf24583 <nrf24583@docomo.ne.jp>
2026-04-22 08:03:23 +0900

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,17 @@
+export function setupTabs() {
+  const homeLink = document.querySelector('[data-tab="home"]');
+  const converterTab = document.querySelector('[data-tab="converter"]');
+  const homeSection = document.getElementById("home");
+  const converterSection = document.getElementById("converter");
+
+  homeLink.addEventListener("click", () => {
+    //console.log("HomeClick");
+    converterSection.classList.add("hidden");
+    homeSection.classList.remove("hidden");
+  });
+
+  converterTab.addEventListener("click", () => {
+    homeSection.classList.add("hidden");
+    converterSection.classList.remove("hidden");
+  });
+}
